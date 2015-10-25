393ff139c23ee6e6f9208595bca7ec02650c8f71
A. Alfonso F. R. <alffore@gmail.com>
2015-10-24 21:32:42 -0500

    Script para ejecución

---
+++ bin/cac6.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#./CAC6.exe 4844 1654 ../datos_ex/salida_localidades_15.txt ../datos_ex/salida_ipc_15.txt
+
+./CAC6.exe 192244 23009 ../datos_ex/salida_localidades.txt ../datos_ex/salida_ipc.txt
+
