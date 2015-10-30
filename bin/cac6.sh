96b21b3e5053e6272c93d4b7854eee809fe0c34f
A. Alfonso F. R <alffore@gmail.com>
2015-10-29 18:14:55 -0600

    Se cambio el script para que se ejecute en el servidor del SIC

--- bin/cac6.sh
+++ bin/cac6.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+locs=`cat /var/www/html/renic/utiles/cac2/salida_localidades.txt|wc -l`
+
+recs=`cat /var/www/html/renic/utiles/cac2/salida_ipc.txt|wc -l`
+
 #./CAC6.exe 4844 1654 ../datos_ex/salida_localidades_15.txt ../datos_ex/salida_ipc_15.txt
 
-./CAC6.exe 192244 23009 ../datos_ex/salida_localidades.txt ../datos_ex/salida_ipc.txt
+./CAC6.exe $recs $locs ../datos_ex/salida_localidades.txt ../datos_ex/salida_ipc.txt
 
