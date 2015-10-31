e498efc9c2cb0a6a552dac686a057e960052414e
A. Alfonso F. R <alffore@gmail.com>
2015-10-31 08:12:46 -0600

    Se agrega el script de ejecución en servidor

---
+++ bin/cac6.sic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+locs=`cat /var/www/html/renic/utiles/cac2/salida_localidades.txt|wc -l`
+
+recs=`cat /var/www/html/renic/utiles/cac2/salida_ipc.txt|wc -l`
+
+#./CAC6.exe 4844 1654 ../datos_ex/salida_localidades_15.txt ../datos_ex/salida_ipc_15.txt
+
+./CAC6.exe $locs $recs /var/www/html/renic/utiles/cac2/salida_localidades.txt /var/www/html/renic/utiles/cac2/salida_ipc.txt 
+
